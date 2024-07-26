6e6b3640bc26493b53e4b74ea5cfda376940f5dd
Amit <ninja208201@gmail.com>
2024-07-26 10:40:38 +0530

    correct the input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,36 +5,37 @@ import { createContext, useContext, useId } from "react";
 const InputContext = createContext();
 
 export function IWraper({ children, ...rest }) {
-    return (
-        <InputContext.Provider value={rest}>
-            <div className="flex gap-4 flex-col">{children}</div>
-        </InputContext.Provider>
-    );
+  return (
+    <InputContext.Provider value={rest}>
+      <div className="flex gap-4 flex-col">{children}</div>
+    </InputContext.Provider>
+  );
 }
 
-function Input({ label, ...rest }) {
-    const { errors } = useContext(InputContext);
-    const id = useId();
+function Input({ label, name, validation, ...rest }) {
+  const {
+    errors,
+    register,
+    validation: defaultValidation,
+  } = useContext(InputContext);
+  const id = useId();
 
-    return (
-        <label htmlFor={id}>
-            <span className="font-semibold text-sm">{label}</span>
-            <div>
-                <input
-                    {...rest}
-                    id={id}
-                    className={
-                        "p-2 border-primary border outline-none rounded-md w-full"
-                    }
-                />
-                {errors?.[rest.name]?.type === "required" && (
-                    <p className="text-red-500 text-xs">
-                        This field is required
-                    </p>
-                )}
-            </div>
-        </label>
-    );
+  return (
+    <label htmlFor={id}>
+      <span className="font-semibold text-sm">{label}</span>
+      <div>
+        <input
+          id={id}
+          className={"p-2 border-primary border outline-none rounded-md w-full"}
+          {...rest}
+          {...register(name, validation || defaultValidation)}
+        />
+        {errors?.[name]?.type === "required" && (
+          <p className="text-red-500 text-xs">This field is required</p>
+        )}
+      </div>
+    </label>
+  );
 }
 
 export default Input;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,52 +3,51 @@ import { useForm } from "react-hook-form";
 import Button from "../components/Button";
 
 const Register = () => {
-    const form = useForm();
+  const form = useForm();
 
-    const {
-        register,
-        formState: { errors },
-        handleSubmit,
-    } = form;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+  } = form;
 
-    function submitHandler(data) {}
+  function submitHandler(data) {
+    console.log(data);
+  }
 
-    return (
-        <div className="w-full h-screen flex">
-            <div className="bg-white flex-[0.6] p-6 flex flex-col gap-16">
-                <h1>Mirror</h1>
-                <div className="">
-                    <h2 className="text-2xl mb-6">Register</h2>
+  return (
+    <div className="w-full h-screen flex">
+      <div className="bg-white flex-[0.6] p-6 flex flex-col gap-16">
+        <h1>Mirror</h1>
+        <div className="">
+          <h2 className="text-2xl mb-6">Register</h2>
 
-                    <form onSubmit={handleSubmit(submitHandler)}>
-                        <IWraper errors={errors}>
-                            <Input
-                                label={"Email"}
-                                type="email"
-                                {...register("email", { required: true })}
-                            />
-                            <Input
-                                label={"Password"}
-                                type="password"
-                                {...register("password", { required: true })}
-                            />
-                            <Input
-                                label={"Re-Type Password"}
-                                type="password"
-                                {...register("confirmPassword", {
-                                    required: true,
-                                })}
-                            />
-                        </IWraper>
+          <form onSubmit={handleSubmit(submitHandler)}>
+            <IWraper
+              errors={errors}
+              register={register}
+              validation={{ required: true }}
+            >
+              <Input label={"Email"} type="email" name="email" />
+              <Input label={"Password"} type="password" name="password" />
+              <Input
+                label={"Re-Type Password"}
+                type="password"
+                name="confirmPassword"
+              />
+            </IWraper>
 
-                        <Button.Right className={"my-4"} type="submit">Save</Button.Right>
-                    </form>
-                </div>
-            </div>
-
-            <div className="bg-green-400 flex-1"></div>
+            <Button.Right className={"my-4"} type="submit">
+              Save
+            </Button.Right>
+          </form>
         </div>
-    );
+      </div>
+
+      <div className="bg-green-400 flex-1"></div>
+    </div>
+  );
 };
 
 export default Register;
